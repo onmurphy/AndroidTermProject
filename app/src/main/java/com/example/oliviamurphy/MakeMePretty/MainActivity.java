b33a794a9625d1b4fc23e501a766c4fa24226651
sep0020 <sep0020@auburn.edu>
2016-04-20 12:34:57 -0500

    Merge branch 'master' of https://github.com/onmurphy/TermProject
    # Conflicts:
    #	app/src/main/java/com/example/oliviamurphy/MakeMePretty/Quiz.java
    #	app/src/main/java/com/example/oliviamurphy/MakeMePretty/QuizResults.java
    
    Change splash text color, remove action bar on "start quiz" page

--- app/src/main/java/com/example/oliviamurphy/MakeMePretty/MainActivity.java
+++ app/src/main/java/com/example/oliviamurphy/MakeMePretty/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.content.Intent;
 
